eb72e51b0bfcd905e9f0ab765b031221e7946c49
BingYaoxu <1138726228@qq.com>
2019-10-29 00:07:19 +0800

    更改版本

--- pull-k8s.sh
+++ pull-k8s.sh
@@ -1,5 +1,5 @@
 images=(kube-proxy-amd64:v1.10.1 kube-scheduler-amd64:v1.10.1 kube-controller-manager-amd64:v1.10.1 kube-apiserver-amd64:v1.10.1 etcd-amd64:3.1.12 pause-amd64:3.1 kubernetes-dashboard-amd64:v1.8.3 k8s-dns-sidecar-amd64:1.14.8 k8s-dns-kube-dns-amd64:1.14.8 k8s-dns-dnsmasq-nanny-amd64:1.14.8)
-goser=registry.cn-hangzhou.aliyuncs.com/google_containers
+goser=registry.cn-hangzhou.aliyuncs.com/kubernetes_in_docker
 for image in ${$images[@]}; do
   docker pull $goser/$image
   docker tag $goser/$image gcr.io/google_containers/$image
